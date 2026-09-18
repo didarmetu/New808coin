a14b18a109345b3b5fe62c965c055bd9d761de1b
didarmetu <didarmetu@gmail.com>
2026-09-18 18:22:40 -0400

    Harden wallet-disabled runtime safety

--- src/rpcmasternode.cpp
+++ src/rpcmasternode.cpp
@@ -19,9 +19,16 @@
 #include <boost/tokenizer.hpp>
 #include <fstream>
 
+static void EnsureMasternodeWalletAvailable()
+{
+    if (!pwalletMain)
+        throw JSONRPCError(RPC_METHOD_NOT_FOUND, "Method not found (wallet disabled)");
+}
 
 void SendMoney(const CTxDestination& address, CAmount nValue, CWalletTx& wtxNew, AvailableCoinsType coin_type = ALL_COINS)
 {
+    EnsureMasternodeWalletAvailable();
+
     // Check amount
     if (nValue <= 0)
         throw JSONRPCError(RPC_INVALID_PARAMETER, "Invalid amount");
@@ -391,6 +398,9 @@ UniValue masternodedebug (const UniValue& params, bool fHelp)
             "\nExamples:\n" +
             HelpExampleCli("masternodedebug", "") + HelpExampleRpc("masternodedebug", ""));
 
+    if (!pwalletMain)
+        return activeMasternode.GetStatus();
+
     if (activeMasternode.status != ACTIVE_MASTERNODE_INITIAL || !masternodeSync.IsSynced())
         return activeMasternode.GetStatus();
 
@@ -447,6 +457,8 @@ UniValue startmasternode (const UniValue& params, bool fHelp)
             "}\n"
             "\nExamples:\n" +
             HelpExampleCli("startmasternode", "\"alias\" \"0\" \"my_mn\"") + HelpExampleRpc("startmasternode", "\"alias\" \"0\" \"my_mn\""));
+
+    EnsureMasternodeWalletAvailable();
 
     bool fLock = (params[1].get_str() == "true" ? true : false);
 
@@ -613,6 +625,8 @@ UniValue getmasternodeoutputs (const UniValue& params, bool fHelp)
 
             "\nExamples:\n" +
             HelpExampleCli("getmasternodeoutputs", "") + HelpExampleRpc("getmasternodeoutputs", ""));
+
+    EnsureMasternodeWalletAvailable();
 
     // Find possible candidates
     vector<COutput> possibleCoins = activeMasternode.SelectCoinsMasternode();
